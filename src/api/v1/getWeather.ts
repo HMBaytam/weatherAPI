bf059a2a49fcc11241649a9c3c37d023804526e2
Hassan Elbaytam <hassan.elbaytam@gmail.com>
2026-02-28 13:35:00 -0500

    refactored `/weather` route to do all of the data cleaning in a utility function

--- src/api/v1/getWeather.ts
+++ src/api/v1/getWeather.ts
@@ -1,44 +1,22 @@
 import { Router, Request, Response } from 'express';
 import { BaseWeatherData } from '../../modules/weather';
-import { getWeatherForCity } from '../../utils/funcs';
+import { baseWeatherFetch, cleanWeatherData } from '../../utils/funcs';
 
 const router = Router();
 let weatherData: BaseWeatherData;
 
 
 // TODO: Add a way to check what unit the user wants with the default being Farenheit and add that to the url in utils/funcs.ts
-// TODO: Clean up this code and export json handleing to a function in utils/funcs.ts
 router.get('/weather', async (req: Request, res: Response) => {
     const city: string = req.query.city as string;
     const country: string = req.query.country as string;
 
-    var allData: any = await getWeatherForCity(city, country);
+    var allData: any = await baseWeatherFetch(city, country);
     var tempData: Array<any> = allData['days'];
 
-    var weatherDetails: Array<any> = [];
 
+    weatherData = cleanWeatherData(allData);
 
-    for (let i=0; i<tempData.length; i++) {
-        weatherDetails.push({
-            date: tempData[i]['datetime'],
-            maxTemp: tempData[i]['tempmax'],
-            minTemp: tempData[i]['tempmin'],
-            tempNow: tempData[i]['temp'],
-            condition: tempData[i]['conditions'],
-            description: tempData[i]['description']
-        })
-    }
-
-
-    weatherData = {
-        cityId: `${allData['latitude']},${allData['longitude']}`,
-        country: allData['resolvedAddress'].split(',')[2].trim(),
-        state: allData['resolvedAddress'].split(',')[1].trim(),
-        city: allData['resolvedAddress'].split(',')[0].trim(),
-        description: allData['description'],
-        weatherDetails: weatherDetails
-        
-    }
     res.json(weatherData);
 })
 
--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -1,10 +1,11 @@
 import * as dotenv from 'dotenv';
+import { BaseWeatherData } from '../modules/weather';
 dotenv.config();
 
 const BASE_URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline';
 const API_KEY = process.env.WEATHER_API_KEY as string;
 
-export function getWeatherForCity(city: string, country: string): Promise<JSON> {
+export function baseWeatherFetch(city: string, country: string): Promise<JSON> {
 
     console.log(`Getting weather data for ${city}, ${country}...`);
 
@@ -17,4 +18,29 @@ export function getWeatherForCity(city: string, country: string): Promise<JSON> 
 
 // TODO: Add a way to convert full country name into country code
 
+export function cleanWeatherData(data: any): any {
+    let responseData: BaseWeatherData;
+    let dailyBreakdown: Array<any> = data['days'];
 
+    for (let i=0; i<data.length; i++) {
+        dailyBreakdown.push({
+            date: data[i]['datetime'],
+            maxTemp: data[i]['tempmax'],
+            minTemp: data[i]['tempmin'],
+            tempNow: data[i]['temp'],
+            condition: data[i]['conditions'],
+            description: data[i]['description']
+        })
+    }
+    responseData = {
+        cityId: `${data['latitude']},${data['longitude']}`,
+        country: data['resolvedAddress'].split(',')[2].trim(),
+        state: data['resolvedAddress'].split(',')[1].trim(),
+        city: data['resolvedAddress'].split(',')[0].trim(),
+        description: data['description'],
+        weatherDetails: dailyBreakdown
+        
+    }
+
+  return responseData;
+}
